4d30ed6fc0224bace265fbc6ff9d9e0a71b95280
Joel Cruz <136981207+JoelCruzCode@users.noreply.github.com>
2025-02-21 21:13:26 -0800

    build: update proxy & target

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   server: {
+    port: 5174,
     proxy: {
-      '/api': 'https://localhost:3000',
-    },
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        secure: false,
+
+      }
+    }
   },
   plugins: [react()],
 })

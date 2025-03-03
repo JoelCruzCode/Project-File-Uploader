19e4498d4c3a5f4ac8d2887fc766f1c5964fd778
Joel Cruz <136981207+JoelCruzCode@users.noreply.github.com>
2025-03-02 20:43:30 -0800

    Add routes for navigation in header components
    Created Header component to display title and navbar to the main layout
    created Routes.tsx to hold path Constants for navbar links

--- client/src/Routes.tsx
+++ client/src/Routes.tsx
@@ -1,3 +1,5 @@
+import { v4 as uuidv4 } from "uuid";
+
 const HOME_ROUTE = "/"
 const LOGIN_ROUTE = "login"
 const UPLOAD_ROUTE = "upload"
@@ -7,6 +9,7 @@ const ACCOUNT_ROUTE = "account"
 export interface Route {
   title: string;
   path: string;
+  id: string;
   auth?: boolean;
 }
 
@@ -14,22 +17,27 @@ export const APP_ROUTES: Record<string, Route> = {
   home: {
     title: "Home",
     path: HOME_ROUTE,
+    id: uuidv4(),
   },
   login: {
     title: "Login",
-    path: LOGIN_ROUTE
+    path: LOGIN_ROUTE,
+    id: uuidv4(),
   },
   upload: {
     title: "Upload",
     path: UPLOAD_ROUTE,
+    id: uuidv4(),
   },
   library: {
     title: "Library",
     path: LIBRARY_ROUTE,
+    id: uuidv4(),
   },
   account: {
     title: "Account",
-    path: ACCOUNT_ROUTE
+    path: ACCOUNT_ROUTE,
+    id: uuidv4(),
   }
 
 }
--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,4 +1,3 @@
-import { v4 as uuidv4 } from "uuid";
 import { NavLink } from "react-router";
 import classes from "./Header.module.css"
 import { APP_ROUTES, Route } from "../../Routes";
@@ -9,8 +8,8 @@ export const Header = ({ hrefs = APP_ROUTES }: { hrefs?: Record<string, Route> }
 		<header className={classes.headerDiv}>
 			<h1>File Uploader</h1>
 			<nav className={classes.navBar}>
-				{Object.entries(hrefs).map(([key, value]) => (
-					<NavLink to={value.path}> {value.title} </NavLink>
+				{Object.entries(hrefs).map(([__, value]) => (
+					<NavLink to={value.path} key={value.id}> {value.title} </NavLink>
 				))}
 
 

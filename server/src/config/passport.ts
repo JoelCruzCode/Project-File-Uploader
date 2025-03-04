6d7d422424da568f0dd1aeeedb38139f08427319
Joel Cruz <136981207+JoelCruzCode@users.noreply.github.com>
2025-03-04 01:28:08 -0800

    feat: Add passport authentication
    Created a login strategy for authenticating users with passport.The callback
    takes the users input and queries the database using Prisma's "getUser" function
    Once authenticated the user is stored in session under req.user

--- server/src/config/passport.ts
+++ server/src/config/passport.ts
@@ -1,45 +1,65 @@
-//const bcrypt = require("bcryptjs");
-//const passport = require("passport");
-//const localStrategy = require("passport-local").Strategy;
-//const { getUser, getUserById } = require("../models/userModel");
-//
-//const loginCallback = async function(username, password, done) {
-//	try {
-//		const user = await getUser(username);
-//		if (!user) {
-//			return done(null, false, {
-//				message: `username "${username}" doesn't exist`,
-//			});
-//		}
-//
-//		const match = await bcrypt.compare(password, user.password_hash);
-//
-//		if (match) {
-//			return done(null, user);
-//		}
-//
-//		return done(null, false, { message: "Incorrect password" });
-//	} catch (error) {
-//		return done(error);
-//	}
-//};
-//
-//const loginStrategy = new localStrategy(loginCallback);
-//
-//passport.use("local-login", loginStrategy);
-//
-//// CB function assigns user.id to the session when passport authenticates
-//passport.serializeUser((user, done) => {
-//	done(null, user.user_id);
-//});
-//// Once authenticated CB function assigns req.user on every request
-//passport.deserializeUser((userId, done) => {
-//	getUserById(userId)
-//		.then((user) => {
-//			done(null, user);
-//		})
-//		.catch((error) => done(error));
-//});
-//
-//module.exports = passport;
-//
+declare global {
+	namespace Express {
+		interface User {
+			id: number;
+			username: string;
+			email: string;
+			passwordHash: string;
+		}
+	}
+}
+
+interface Done {
+	(error: any, user?: Express.User | false, options?: { message: string }): void;
+}
+
+import bcrypt from "bcryptjs"
+import passport from "passport";
+import { Strategy as LocalStrategy } from "passport-local";
+import { getUser } from "../models/userModel.ts";
+
+
+const loginCallBack = async function(username: string, password: string, done: Done) {
+	try {
+		const user = await getUser(username)
+		if (!user) {
+			return done(null, false, {
+				message: `Username "${username}" doesn't exist`
+			}
+			)
+		}
+
+		const match = await bcrypt.compare(password, user?.passwordHash);
+
+		if (match) {
+			return done(null, user);
+		}
+
+		return done(null, false, { message: "Incorrect password" })
+
+	} catch (error) {
+		return done(error)
+	}
+}
+
+const loginStrategy = new LocalStrategy(loginCallBack)
+
+passport.use("local-login", loginStrategy)
+
+// CB function assigns user.id to the session when passport authenticates
+passport.serializeUser((user: Express.User, done) => {
+	done(null, user.username);
+})
+
+
+// Once authenticated CB function assigns req.user on every request
+passport.deserializeUser(async (username: string, done) => {
+	getUser(username)
+		.then((user) => {
+			done(null, user);
+
+		}).catch((error) => done(error));
+
+})
+
+export default passport
